6c50997d1a398901afdda649a490c95a0620ea0a
JDavidLL <davidlopez1415@hotmail.com>
2025-04-17 14:08:18 -0600

    Delete src directory

--- src/main/java/com/crud/card/repository/IEstadisticaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.crud.card.repository;
-
-import com.crud.card.model.EstadisticaEdad;
-
-import java.util.List;
-
-public interface IEstadisticaRepository {
-    List<EstadisticaEdad> obtenerEstadisticas();
-}
